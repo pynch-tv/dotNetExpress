8681d20704e0c781004c109a84aee2c7f138737e
lathoub <4082369+lathoub@users.noreply.github.com>
2024-12-08 12:59:26 +0100

    fixed issue 3
    https://github.com/lathoub/dotNetExpress/issues/3

--- dotNetExpress/ClientConnection.cs
+++ dotNetExpress/ClientConnection.cs
@@ -11,12 +11,14 @@ internal class Client
     {
         try
         {
+            if (!tcpClient.Connected)
+                return;
+
             var stream = tcpClient.GetStream();
 
             while (tcpClient.Connected)
             {
-
-                Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] waiting to make a Request object");
+                Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) ({DateTime.Now:HH.mm.ss:ffff}) waiting to make a Request object");
 
                 if (!GetRequest(express, tcpClient, out Request req))
                     throw new HttpProtocolException(500, "Unable to construct Request", new ProtocolViolationException("Unable to construct Request"));
@@ -24,7 +26,7 @@ internal class Client
                 if (req == null || req.Method == null)
                     throw new HttpProtocolException(500, "Error while parsing reuqest", new ProtocolViolationException("Unable to construct Request"));
 
-                Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] We have a Request object");
+                Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) We have a Request object");
 
                 stream.ReadTimeout = express.KeepAliveTimeout * 1000; 
 
@@ -43,12 +45,12 @@ internal class Client
 
                 if (req.Res.Get("Upgrade") != null && req.Res.Get("Upgrade").Equals("WebSocket", StringComparison.OrdinalIgnoreCase))
                 {
-                    Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] websocket upgrade");
+                    Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) websocket upgrade");
                     break; // websocket
                 }
                 else if (req.Res.Get("Content-Type") != null && req.Res.Get("Content-Type").Equals("text/event-stream", StringComparison.OrdinalIgnoreCase))
                 {
-                    Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] text/event-stream");
+                    Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) text/event-stream");
                     break; // Server-Sent Events
                 }
                 else if (req.Res.Get("Connection") != null && req.Res.Get("Connection").Equals("keep-alive", StringComparison.OrdinalIgnoreCase))
@@ -56,13 +58,13 @@ internal class Client
                     // https://learn.microsoft.com/en-us/dotnet/api/system.net.sockets.tcplistener.accepttcpclient?view=net-8.0
                     // Remark: When you are through with the TcpClient, be sure to call its Close method. If you want greater
                     // flexibility than a TcpClient offers, consider using AcceptSocket.
-                    Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] Lets keep the connection open");
+                    Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) Lets keep the connection open");
                     continue;
                 }
                 else
                 {
                     // Do not keep the connection alive, leave the while loop
-                    Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] Connection does not need to be kept open");
+                    Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) Connection does not need to be kept open");
                     tcpClient.Close();
                     break;
                 }
@@ -70,7 +72,7 @@ internal class Client
         }
         catch (Exception e)
         {
-            Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] Exception {e.Message}");
+            Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) Exception {e.Message}");
             tcpClient.Close();
         }
     }
@@ -145,10 +147,10 @@ internal class Client
             }
         }
 
-        Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] {req.Protocol} {req.HttpVersion} {req.Path}");
-        Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] Headers:");
+        Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) {req.Protocol} {req.HttpVersion} {req.Path}");
+        Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) Headers:");
         foreach (string header in req.Headers)
-            Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] \t{header}: {req.Headers[header]}");
+            Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) \t{header}: {req.Headers[header]}");
 
         req.Host = req.Headers["host"];
         req.Hostname = req.Headers["host"]?.Split(':')[0];
--- dotNetExpress/Server.cs
+++ dotNetExpress/Server.cs
@@ -13,10 +13,6 @@ public class Server : TcpListener
 
     private CancellationTokenSource _cancellation = new();
 
-    private object _lock = new();
-
-    private int connectionCount = 0;
-
     #region Constructor
 
     /// <summary>
@@ -57,31 +53,17 @@ public class Server : TcpListener
     /// <summary>
     /// 
     /// </summary>
-    /// <param name="tcpClient"></param>
-    private void RaiseHandleConnection(TcpClient tcpClient)
+    /// <param name="connection"></param>
+    private void RaiseHandleConnection(TcpClient connection)
     {
-        lock (_lock)
-        {
-            connectionCount++;
-        }
-
-        Debug.WriteLine($"[T{Environment.CurrentManagedThreadId}] (Total open: {connectionCount}) RaiseHandleConnection from {tcpClient.Client.RemoteEndPoint}");
-
         var handler = HandleConnection;
-        handler?.Invoke(this, tcpClient);
-
-        lock (_lock)
-        {
-            connectionCount--;
-        }
-
-        Debug.WriteLine($"[T{Environment.CurrentManagedThreadId}] (Still open: {connectionCount}) Connection done {tcpClient.Client?.RemoteEndPoint} {tcpClient.Client?.IsBound}");
+        handler?.Invoke(this, connection);
     }
 
     #endregion
 
     /// <summary>
-    /// 
+    /// https://www.codeproject.com/Articles/5270779/High-Performance-TCP-Client-Server-using-TCPListen
     /// </summary>
     /// <param name="express"></param>
     public async Task Begin(Express express)
@@ -90,62 +72,70 @@ public class Server : TcpListener
 
         _tcpListenerThread = new Thread(() =>
         {
-            Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] Listener started");
+            Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) Listener started");
 
             List<Task> tcpClientTasks = [];
-            int awaiterTimeoutInMS = 500;
+            int awaiterTimeoutInMS = 50;
 
-            var keepGoing = true;
-
-            _cancellation.Token.Register(() =>
-            {
-                keepGoing = false;
-            });
+            _cancellation.Token.Register(() => this.Stop());
 
             try
             {
                 // maintain a max amount of awaiting connections. A closed connection
                 // is immediately replace by an awaiting connection.
-                while (keepGoing)
+                while (true)
                 {
-                    while (keepGoing && tcpClientTasks.Count < express.MaxConcurrentListeners)
+                    if (_cancellation.Token.IsCancellationRequested)
+                        break;
+
+                    while (tcpClientTasks.Count < express.MaxConcurrentListeners)
                     {
                         var awaiterTask = Task.Run(async () =>
                         {
                             try
                             {
-                                Debug.WriteLine($"[T{Task.CurrentId}] Awaiting new connection");
+                                Debug.WriteLine($"[T{Task.CurrentId}] ({DateTime.Now:HH.mm.ss:ffff}) Awaiting new connection");
 
                                 RaiseHandleConnection(await AcceptTcpClientAsync(_cancellation.Token));
                             }
                             catch (Exception e)
                             {
-                                Debug.WriteLine($"[T{Task.CurrentId}] Error in Client.Connection: {e.Message}");
-                                keepGoing = false;
+                                Debug.WriteLine($"[T{Task.CurrentId}] ({DateTime.Now:HH.mm.ss:ffff}) Error in Client.Connection: {e.Message}");
                             }
                         });
-                        Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] added task [T{awaiterTask.Id}]");
+                        Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) added task [T{awaiterTask.Id}]");
                         tcpClientTasks.Add(awaiterTask);
                     }
 
-                    var removeAtIndex = Task.WaitAny([.. tcpClientTasks], awaiterTimeoutInMS);
-                    if (removeAtIndex > 0)
+                    // Waits for any of the provided Task objects to complete execution.
+                   // while (true)
                     {
-                        Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] removing connection at index {removeAtIndex} with Task Id: [T{tcpClientTasks[removeAtIndex].Id}]");
-                        tcpClientTasks.RemoveAt(removeAtIndex);
+                        var i = Task.WaitAny([.. tcpClientTasks], awaiterTimeoutInMS);
+                        if (i >= 0)
+                        {
+
+                            Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) removing connection at index {i} with Task Id: [T{tcpClientTasks[i].Id}]");
+                            tcpClientTasks.RemoveAt(i);
+                        }
                     }
+
+                    foreach (var task in tcpClientTasks)
+                        if (task.Status == TaskStatus.RanToCompletion)
+                            Debug.Assert(1==0, "************************************* some tasks ran to completion, but where not removed");
                 }
             }
+            catch (OperationCanceledException) { throw; }
             catch (Exception e) 
             {
-                Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] Inner loop exception: {e.Message}");
+                Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) Inner loop exception: {e.Message}");
+                _cancellation.Token.ThrowIfCancellationRequested();
             }
 
-            Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] Listener stopped");
+            Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) Listener stopped");
         });
 
         _tcpListenerThread.Start();
-        Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] Listener starting");
+        Debug.WriteLine($"[{Environment.CurrentManagedThreadId}] ({DateTime.Now:HH.mm.ss:ffff}) Listener starting");
     }
 
     /// <summary>
@@ -156,8 +146,6 @@ public class Server : TcpListener
         _cancellation.Cancel();
 
         Debug.WriteLine("Listener stopping");
-
-        Stop();
     }
 
 }
